24cc44ec6e2d4d6d3ab41fe9f786b60eacc92033
Rahul Prajapati <132542774+rahulprajapati08@users.noreply.github.com>
2024-09-04 15:02:54 +0530

    new Update bedapi.py

--- bedapi.py
+++ bedapi.py
@@ -1,5 +1,6 @@
 import random
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,5 @@ def get_hospitals_data():
     return jsonify(hospitals_data)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
